78e0eb332ad4af4012772169ce8dae9b3d3b113d
asuka <asuka.altair@gmail.com>
2020-07-05 16:18:03 +0900

    [finish] task4

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,18 @@ Rails.application.routes.draw do
 
   root 'homes#top'
   get 'home/about' => "homes#about"
+
   resources :users, only: [:show,:index,:edit,:update]
+
+  post 'follow/:id' => 'relationships#create', as: 'follow'
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow'
+  get 'users/:id/follows' => 'relationships#follows', as: 'follows'
+  get 'users/:id/followers' => 'relationships#followers', as: 'followers'
+
   resources :books do
   	resource :favorites, only: [:create, :destroy]
   	resources :post_comments, only: [:create, :destroy]
   end
+
 
 end
